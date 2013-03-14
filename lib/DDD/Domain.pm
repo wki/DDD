51aaee5f9c45af42b990cc624b742a49a886cc1b
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2013-03-14 23:36:53 +0100

    fixed typos in DDD::Domain

--- lib/DDD/Domain.pm
+++ lib/DDD/Domain.pm
@@ -1,39 +1,42 @@
 package DDD::Domain;
 use Moose ();
 use Moose::Exporter;
-use Sub::Install 'install_sub';
+use Sub::Install;
 
 extends 'Bread::Board::Declare';
 
 Moose::Exporter->setup_import_methods(
-    with_meta => [ qw(aggregate service) ],
-    also      => 'Moose',
+    with_meta => ['aggregate', 'service'],
+    also      => ['Moose', 'Bread::Board::Declare'],
 );
 
 sub aggregate {
     my ($meta, $name, @args) = @_;
-    
-    Moose::has($meta, "_$name", is => 'ro', @args);
 
     my $package = caller;
     
-    install_sub({
-      code => sub {
-          my $self = shift;
+    # _name attribute as a service
+    Moose::has($meta, "_$name", is => 'ro', @args);
 
-          return $self->resolve(
-              service    => "_$name",
-              parameters => { @_ },
-          );
-      },
-      into => $package,
-      as   => $name
+    # name method as accessor
+    Sub::Install::install_sub({
+        code => sub {
+            my $self = shift;
+            
+            return $self->resolve(
+                service    => "_$name",
+                parameters => { @_ },
+            );
+        },
+        into => $package,
+        as   => $name
     });
 }
 
 sub service {
     my ($meta, $name, @args) = @_;
     
+    # name attribute as a service
     Moose::has($meta, $name, is => 'ro', lifecycle => 'Singleton', @args);
 }
 
